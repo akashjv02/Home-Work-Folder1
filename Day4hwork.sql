09da8c6f4d6dc0b23693726d0e9dc6e517cb252d
akashjv02 <akashjv02@gmail.com>
2025-09-22 21:34:40 +0530

    Fix comment typo in SQL script for clarity

--- Day4hwork.sql
+++ Day4hwork.sql
@@ -4,7 +4,7 @@ VALUES (1, 'Alice', 'Web Development', 5000, 'Inactive'),
 (2, 'Bob', 'Data Science', 7000, 'Inactive'),
 (3, 'Charlie', 'UI/UX Design', 4000, 'Active');
 
---Question Queries--
+--Question's Answer Queries--
 
 SELECT * FROM students
 WHERE fees_paid > 5000;
