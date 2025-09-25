4f6fc579007142801a73c194152e3d67194b1f27
akashjv02 <akashjv02@gmail.com>
2025-09-25 20:23:56 +0530

    Add SQL query to retrieve users with the same challenge as 'Farah'

--- Day7hwork.sql
+++ Day7hwork.sql
@@ -10,3 +10,8 @@ VALUES (1,'Raj','Chennai',88,5,'Fitness'),
 --Show the details of users whose score is greater than the average score across all users.--
 SELECT * FROM user
 WHERE score > (SELECT AVG(score) FROM user);
+
+--show same challenge users as (usernamed 'FArah')--
+SELECT name
+FROM user
+WHERE challenge = (SELECT challenge FROM user WHERE name = 'Farah');
