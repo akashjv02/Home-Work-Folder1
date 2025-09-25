357a4535654ed928b0cd5995322e88ad1abb98ea
akashjv02 <akashjv02@gmail.com>
2025-09-25 16:03:34 +0530

    Add SQL script to insert user data and query for high-scoring users

---
+++ Day7hwork.sql
@@ -0,0 +1,12 @@
+--insert data into the table--
+INSERT INTO user(id,name,city,score,bonus,challenge)
+VALUES (1,'Raj','Chennai',88,5,'Fitness'),
+(2,'Anu','Mumbai',91,NULL,'Diet'),
+(3,'Ravi','Chennai',78,3,'Fitness'),
+(4,'Meena','Delhi',82,NULL,'Diet'),
+(5,'Farah','Mumbai',95,4,'Fitness'),
+(6,'Kiran','Pune',70,NULL,'Yoga'),
+(7,'Latha','Pune',87,NULL,'Fitness');
+--Show the details of users whose score is greater than the average score across all users.--
+SELECT * FROM user
+WHERE score > (SELECT AVG(score) FROM user);
